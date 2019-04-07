1fa9a84d000b049c34c875a79567b47614daa13d
James <james_morris1@mail.com>
2019-04-07 04:57:25 +0100

    Fix

--- app/src/main/java/com/example/james/runningman/database/LocationProviderContract.java
+++ app/src/main/java/com/example/james/runningman/database/LocationProviderContract.java
@@ -14,7 +14,7 @@ import android.net.Uri;
 
 public class LocationProviderContract {
 
-    public static final String AUTHORITY = "com.example.james.runningman.database.LocationProvider";
+    public static final String AUTHORITY = "com.example.james.runningman.database";
     public static final String TABLE_NAME = "locations";
 
     public static final Uri LOCATIONS_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE_NAME);
